9934cc053ff72c4ce26df3d0eb60e9f4d1cf2ea6
zhangsf <ynkjmacmini1@mini.local>
2017-02-21 14:33:12 +0800

    first app calculator

---
+++ Calculator_SF/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Calculator_SF
+//
+//  Created by YNKJMACMINI1 on 17/2/20.
+//  Copyright © 2017年 YNKJMACMINI1. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
